d23e696d40ba82a8a0e1e5f8db5b07fb1255dd74
richkirl <loliiwantlllwnt@gmail.com>
2021-04-17 19:24:17 +0300

    0.5.3 Update(prepare OBJLoading)

--- src/com/gamengine/Engine.java
+++ src/com/gamengine/Engine.java
@@ -42,34 +42,94 @@ public class Engine {
 		StaticShader staticshader = new StaticShader();
 		Renderer renderer = new Renderer(window,staticshader);
 		float[] vertices = {
-	    		-0.5f, 0.5f, 0.0f, //
-	    		-0.5f, -0.5f, 0.0f, //
-	    		0.5f, -0.5f, 0.0f,  //
-				0.5f, 0.5f, 0.0f   //
+				-0.5f,0.5f,0,
+				-0.5f,-0.5f,0,
+				0.5f,-0.5f,0,
+				0.5f,0.5f,0,
 
-	    };
-		int[] indices = {
-				0,1,3, //
-				3,1,2  //
+				-0.5f,0.5f,1,
+				-0.5f,-0.5f,1,
+				0.5f,-0.5f,1,
+				0.5f,0.5f,1,
+
+				0.5f,0.5f,0,
+				0.5f,-0.5f,0,
+				0.5f,-0.5f,1,
+				0.5f,0.5f,1,
+
+				-0.5f,0.5f,0,
+				-0.5f,-0.5f,0,
+				-0.5f,-0.5f,1,
+				-0.5f,0.5f,1,
+
+				-0.5f,0.5f,1,
+				-0.5f,0.5f,0,
+				0.5f,0.5f,0,
+				0.5f,0.5f,1,
+
+				-0.5f,-0.5f,1,
+				-0.5f,-0.5f,0,
+				0.5f,-0.5f,0,
+				0.5f,-0.5f,1
+
 		};
 
 		float[] textureCoords = {
+
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0,
+				0,0,
+				0,1,
+				1,1,
+				1,0,
 				0,0,
 				0,1,
 				1,1,
 				1,0
+
+
+		};
+
+		int[] indices = {
+				0,1,3,
+				3,1,2,
+				4,5,7,
+				7,5,6,
+				8,9,11,
+				11,9,10,
+				12,13,15,
+				15,13,14,
+				16,17,19,
+				19,17,18,
+				20,21,23,
+				23,21,22
+
 		};
 
 	    RawModel model = loader.loadToVao(vertices,textureCoords,indices);
 		Modeltexture texture = new Modeltexture(loader.loadTexture("grass1.png"));
 		TexturedModel texturedModel = new TexturedModel(model,texture);
-		Entity entity = new Entity(texturedModel,new Vector3f(0,0,1),0,0,0,1);
+		Entity entity = new Entity(texturedModel,new Vector3f(0,0,-3),0,0,0,1);
 		Camera camera = new Camera(window);
 		while(!GLFW.glfwWindowShouldClose(window.getWindow())) {
 
-			entity.increasePosition(0,0,-0.002f);
+			//entity.increasePosition(0,0,-0.002f);
 			camera.move(window);
-			//entity.increaseRotation(0,0,1);
+			entity.increaseRotation(1,1,0);
 			callbacks.processInput(window.getWindow());
 			
 			renderer.prepare();
